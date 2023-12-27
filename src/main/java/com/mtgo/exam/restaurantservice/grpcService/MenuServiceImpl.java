a4750a63b94ab4b8b0407858e99ec819e7995cf7
Theagns <markus.agnsgaard@hotmail.com>
2023-12-27 11:23:38 +0100

    gRPC out commented

--- src/main/java/com/mtgo/exam/restaurantservice/grpcService/MenuServiceImpl.java
+++ src/main/java/com/mtgo/exam/restaurantservice/grpcService/MenuServiceImpl.java
@@ -1,39 +1,39 @@
-package com.mtgo.exam.restaurantservice.grpcService;
-
-import com.mtgo.exam.restaurantservice.dto.MenuItemResponse;
-import com.mtgo.exam.restaurantservice.grpc.MenuRequest;
-import com.mtgo.exam.restaurantservice.grpc.MenuResponse;
-import com.mtgo.exam.restaurantservice.grpc.Item;
-import com.mtgo.exam.restaurantservice.grpc.MenuServiceGrpc;
-import io.grpc.stub.StreamObserver;
-import lombok.RequiredArgsConstructor;
-import net.devh.boot.grpc.server.service.GrpcService;
-
-import com.mtgo.exam.restaurantservice.service.MenuItemService;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RequiredArgsConstructor
-@GrpcService
-@EnableAutoConfiguration
-public class MenuServiceImpl extends MenuServiceGrpc.MenuServiceImplBase {
-    private final MenuItemService menuItemService;
-    public void menu(MenuRequest request, StreamObserver<MenuResponse> responseObserver) {
-        String restaurantID = request.getRestaurantID();
-        List<MenuItemResponse> items = menuItemService.getMenuItemsByRestaurantId(restaurantID);
-        List<Item> responseItems = new ArrayList<>();
-        for (MenuItemResponse item: items) {
-            responseItems.add(Item.newBuilder()
-                    .setItemID(item.getId())
-                    .setName(item.getName())
-                    .build());
-        }
-        MenuResponse res = MenuResponse.newBuilder()
-                .addAllItem(responseItems)
-                .build();
-        responseObserver.onNext(res);
-        responseObserver.onCompleted();
-    }
-}
+//package com.mtgo.exam.restaurantservice.grpcService;
+//
+//import com.mtgo.exam.restaurantservice.dto.MenuItemResponse;
+//import com.mtgo.exam.restaurantservice.grpc.MenuRequest;
+//import com.mtgo.exam.restaurantservice.grpc.MenuResponse;
+//import com.mtgo.exam.restaurantservice.grpc.Item;
+//import com.mtgo.exam.restaurantservice.grpc.MenuServiceGrpc;
+//import io.grpc.stub.StreamObserver;
+//import lombok.RequiredArgsConstructor;
+//import net.devh.boot.grpc.server.service.GrpcService;
+//
+//import com.mtgo.exam.restaurantservice.service.MenuItemService;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@GrpcService
+//@EnableAutoConfiguration
+//public class MenuServiceImpl extends MenuServiceGrpc.MenuServiceImplBase {
+//    private final MenuItemService menuItemService;
+//    public void menu(MenuRequest request, StreamObserver<MenuResponse> responseObserver) {
+//        String restaurantID = request.getRestaurantID();
+//        List<MenuItemResponse> items = menuItemService.getMenuItemsByRestaurantId(restaurantID);
+//        List<Item> responseItems = new ArrayList<>();
+//        for (MenuItemResponse item: items) {
+//            responseItems.add(Item.newBuilder()
+//                    .setItemID(item.getId())
+//                    .setName(item.getName())
+//                    .build());
+//        }
+//        MenuResponse res = MenuResponse.newBuilder()
+//                .addAllItem(responseItems)
+//                .build();
+//        responseObserver.onNext(res);
+//        responseObserver.onCompleted();
+//    }
+//}
--- src/test/java/com/mtgo/exam/restaurantservice/grpcService/MenuServiceImplTest.java
+++ src/test/java/com/mtgo/exam/restaurantservice/grpcService/MenuServiceImplTest.java
@@ -1,130 +1,130 @@
-package com.mtgo.exam.restaurantservice.grpcService;
-
-import com.mtgo.exam.restaurantservice.dto.MenuItemResponse;
-import com.mtgo.exam.restaurantservice.grpc.MenuRequest;
-import com.mtgo.exam.restaurantservice.grpc.MenuResponse;
-import com.mtgo.exam.restaurantservice.model.Restaurant;
-import com.mtgo.exam.restaurantservice.respoitory.MenuItemRepository;
-import com.mtgo.exam.restaurantservice.service.MenuItemService;
-import io.grpc.internal.testing.StreamRecorder;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest
-class MenuServiceImplTest {
-
-
+//package com.mtgo.exam.restaurantservice.grpcService;
 //
-//    Logic performed by the menu grpc server is tested here
-
-    @Mock
-    private MenuItemRepository menuItemRepository1;
-
-    @Mock
-    private MenuItemService menuItemService1;
-
-    @InjectMocks
-    private MenuItemService menuItemService;
-
-    @InjectMocks
-    private MenuServiceImpl menuService;
-
-    @Test
-    void receiveZeroMenuItems() throws Exception {
-        String restaurantId = "id";
-
-        List<MenuItemResponse> items = new ArrayList<>();
-
-        Mockito.when(menuItemService1.getMenuItemsByRestaurantId(restaurantId))
-                .thenReturn(items);
-
-        MenuRequest menuRequest = MenuRequest.newBuilder()
-                .setRestaurantID(restaurantId)
-                .build();
-
-        StreamRecorder<MenuResponse> responseObserver = StreamRecorder.create();
-
-        menuService.menu(menuRequest, responseObserver);
-        //menuItemService.getMenuItemsByRestaurantId(restaurantId);
-
-        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
-            fail("The call did not terminate in time");
-        }
-        assertNull(responseObserver.getError());
-        List<MenuResponse> results = responseObserver.getValues();
-        assertEquals(1, results.size());
-        MenuResponse response = results.get(0);
-        assertEquals(0, response.getItemCount());
-    }
-
-    @Test
-    void receiveMultipleMenuItems() throws Exception {
-        String restaurantId = "id";
-
-        List<MenuItemResponse> items = new ArrayList<>();
-        items.add(new MenuItemResponse("id1", "testItem1", "discription of item 1", new BigDecimal(10) , new Restaurant()));
-        items.add(new MenuItemResponse("id2", "testItem2", "discription of item 1", new BigDecimal(10) , new Restaurant()));
-
-        Mockito.when(menuItemService1.getMenuItemsByRestaurantId(restaurantId))
-                .thenReturn(items);
-
-        MenuRequest menuRequest = MenuRequest.newBuilder()
-                .setRestaurantID(restaurantId)
-                .build();
-
-        StreamRecorder<MenuResponse> responseObserver = StreamRecorder.create();
-
-        menuService.menu(menuRequest, responseObserver);
-        //menuItemService.getMenuItemsByRestaurantId(restaurantId);
-
-        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
-            fail("The call did not terminate in time");
-        }
-        assertNull(responseObserver.getError());
-        List<MenuResponse> results = responseObserver.getValues();
-        assertEquals(1, results.size());
-        MenuResponse response = results.get(0);
-        assertEquals(2, response.getItemCount());
-    }
-
-    @Test
-    void parameterMappingMenuItemToItem() throws Exception {
-        String restaurantId = "id";
-
-        List<MenuItemResponse> items = new ArrayList<>();
-        items.add(new MenuItemResponse("id1", "testItem1", "discription of item 1", new BigDecimal(10) , new Restaurant()));
-
-        Mockito.when(menuItemService1.getMenuItemsByRestaurantId(restaurantId))
-                .thenReturn(items);
-
-        MenuRequest menuRequest = MenuRequest.newBuilder()
-                .setRestaurantID(restaurantId)
-                .build();
-
-        StreamRecorder<MenuResponse> responseObserver = StreamRecorder.create();
-
-        menuService.menu(menuRequest, responseObserver);
-        //menuItemService.getMenuItemsByRestaurantId(restaurantId);
-
-        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
-            fail("The call did not terminate in time");
-        }
-        assertNull(responseObserver.getError());
-        List<MenuResponse> results = responseObserver.getValues();
-        MenuResponse response = results.get(0);
-        assertEquals("id1", response.getItem(0).getItemID());
-    }
-
-}
+//import com.mtgo.exam.restaurantservice.dto.MenuItemResponse;
+//import com.mtgo.exam.restaurantservice.grpc.MenuRequest;
+//import com.mtgo.exam.restaurantservice.grpc.MenuResponse;
+//import com.mtgo.exam.restaurantservice.model.Restaurant;
+//import com.mtgo.exam.restaurantservice.respoitory.MenuItemRepository;
+//import com.mtgo.exam.restaurantservice.service.MenuItemService;
+//import io.grpc.internal.testing.StreamRecorder;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.math.BigDecimal;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.concurrent.TimeUnit;
+//
+//import static org.junit.Assert.assertNull;
+//import static org.junit.Assert.fail;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//@SpringBootTest
+//class MenuServiceImplTest {
+//
+//
+////
+////    Logic performed by the menu grpc server is tested here
+//
+//    @Mock
+//    private MenuItemRepository menuItemRepository1;
+//
+//    @Mock
+//    private MenuItemService menuItemService1;
+//
+//    @InjectMocks
+//    private MenuItemService menuItemService;
+//
+//    @InjectMocks
+//    private MenuServiceImpl menuService;
+//
+//    @Test
+//    void receiveZeroMenuItems() throws Exception {
+//        String restaurantId = "id";
+//
+//        List<MenuItemResponse> items = new ArrayList<>();
+//
+//        Mockito.when(menuItemService1.getMenuItemsByRestaurantId(restaurantId))
+//                .thenReturn(items);
+//
+//        MenuRequest menuRequest = MenuRequest.newBuilder()
+//                .setRestaurantID(restaurantId)
+//                .build();
+//
+//        StreamRecorder<MenuResponse> responseObserver = StreamRecorder.create();
+//
+//        menuService.menu(menuRequest, responseObserver);
+//        //menuItemService.getMenuItemsByRestaurantId(restaurantId);
+//
+//        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+//            fail("The call did not terminate in time");
+//        }
+//        assertNull(responseObserver.getError());
+//        List<MenuResponse> results = responseObserver.getValues();
+//        assertEquals(1, results.size());
+//        MenuResponse response = results.get(0);
+//        assertEquals(0, response.getItemCount());
+//    }
+//
+//    @Test
+//    void receiveMultipleMenuItems() throws Exception {
+//        String restaurantId = "id";
+//
+//        List<MenuItemResponse> items = new ArrayList<>();
+//        items.add(new MenuItemResponse("id1", "testItem1", "discription of item 1", new BigDecimal(10) , new Restaurant()));
+//        items.add(new MenuItemResponse("id2", "testItem2", "discription of item 1", new BigDecimal(10) , new Restaurant()));
+//
+//        Mockito.when(menuItemService1.getMenuItemsByRestaurantId(restaurantId))
+//                .thenReturn(items);
+//
+//        MenuRequest menuRequest = MenuRequest.newBuilder()
+//                .setRestaurantID(restaurantId)
+//                .build();
+//
+//        StreamRecorder<MenuResponse> responseObserver = StreamRecorder.create();
+//
+//        menuService.menu(menuRequest, responseObserver);
+//        //menuItemService.getMenuItemsByRestaurantId(restaurantId);
+//
+//        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+//            fail("The call did not terminate in time");
+//        }
+//        assertNull(responseObserver.getError());
+//        List<MenuResponse> results = responseObserver.getValues();
+//        assertEquals(1, results.size());
+//        MenuResponse response = results.get(0);
+//        assertEquals(2, response.getItemCount());
+//    }
+//
+//    @Test
+//    void parameterMappingMenuItemToItem() throws Exception {
+//        String restaurantId = "id";
+//
+//        List<MenuItemResponse> items = new ArrayList<>();
+//        items.add(new MenuItemResponse("id1", "testItem1", "discription of item 1", new BigDecimal(10) , new Restaurant()));
+//
+//        Mockito.when(menuItemService1.getMenuItemsByRestaurantId(restaurantId))
+//                .thenReturn(items);
+//
+//        MenuRequest menuRequest = MenuRequest.newBuilder()
+//                .setRestaurantID(restaurantId)
+//                .build();
+//
+//        StreamRecorder<MenuResponse> responseObserver = StreamRecorder.create();
+//
+//        menuService.menu(menuRequest, responseObserver);
+//        //menuItemService.getMenuItemsByRestaurantId(restaurantId);
+//
+//        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+//            fail("The call did not terminate in time");
+//        }
+//        assertNull(responseObserver.getError());
+//        List<MenuResponse> results = responseObserver.getValues();
+//        MenuResponse response = results.get(0);
+//        assertEquals("id1", response.getItem(0).getItemID());
+//    }
+//
+//}
