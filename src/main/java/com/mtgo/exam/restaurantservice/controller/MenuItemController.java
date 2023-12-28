0708100f7669e99d800f3f223801715b6d49db80
Thomas <cph-ta181@cphbusiness.dk>
2023-12-28 12:53:41 +0100

    adds currency convertion to menuItems/getAll(?currency={CURRENCY})

--- src/main/java/com/mtgo/exam/restaurantservice/controller/MenuItemController.java
+++ src/main/java/com/mtgo/exam/restaurantservice/controller/MenuItemController.java
@@ -4,13 +4,17 @@ import com.mtgo.exam.restaurantservice.dto.MenuItemRequest;
 import com.mtgo.exam.restaurantservice.dto.MenuItemResponse;
 import com.mtgo.exam.restaurantservice.service.MenuItemService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/restaurants")
 @RequiredArgsConstructor
+@Slf4j
 public class MenuItemController {
 
     private final MenuItemService menuItemService;
@@ -23,9 +27,16 @@ public class MenuItemController {
 
     @GetMapping("/menuItem/getAll")
     @ResponseStatus(HttpStatus.OK)
+    public List<MenuItemResponse> getAllMenuItemsInCurrency(@RequestParam(required = false) String currency){
+        return menuItemService.getAllMenuItemsInCurrency(currency);
+    }
+
+    /*@GetMapping("/menuItem/getAll")
+    @ResponseStatus(HttpStatus.OK)
     public List<MenuItemResponse> getAllMenuItems(){
         return menuItemService.getAllMenuItems();
-    }
+    }*/
+
     @GetMapping("menuItem/byRestaurant")
     @ResponseStatus(HttpStatus.OK)
     public List<MenuItemResponse> getMenuItemsByRestaurantId(@RequestParam String restaurantId) {
--- src/main/java/com/mtgo/exam/restaurantservice/service/MenuItemService.java
+++ src/main/java/com/mtgo/exam/restaurantservice/service/MenuItemService.java
@@ -1,5 +1,8 @@
 package com.mtgo.exam.restaurantservice.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mtgo.exam.restaurantservice.dto.MenuItemRequest;
 import com.mtgo.exam.restaurantservice.dto.MenuItemResponse;
 import com.mtgo.exam.restaurantservice.model.MenuItem;
@@ -7,19 +10,19 @@ import com.mtgo.exam.restaurantservice.model.Restaurant;
 import com.mtgo.exam.restaurantservice.respoitory.MenuItemRepository;
 import com.mtgo.exam.restaurantservice.respoitory.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
+import java.math.BigDecimal;
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor //laver vores menuitemrespoity construtor når man compiler
@@ -41,6 +44,45 @@ public class MenuItemService {
         menuItemRepository.save(menuItem);
         log.info("Menu item {} is saved", menuItem.getId(), restaurantId);
 
+    }
+
+    public List<MenuItemResponse> getAllMenuItemsInCurrency(String currency){
+        List<MenuItem> menuItems = menuItemRepository.findAll();
+        if (currency != null){
+            double rate = getCurrencyConvertion(currency);
+            for (MenuItem item: menuItems) {
+                item.setPrice(item.getPrice().multiply(new BigDecimal(rate)));
+            }
+        }
+        return menuItems.stream().map(this::mapToMenuItemResponse).toList();
+    }
+
+    private double getCurrencyConvertion(String currency){
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_0dFB9lxrvoPe1l392xGkRWZJFfxpnv3uCFNoYnwo&currencies=" + currency + "&base_currency=DKK"))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        try {
+            // Parse JSON string to JsonNode
+            JsonNode jsonNode = objectMapper.readTree(response.body());
+
+            // Extract value
+            double exchangeRate = jsonNode.get("data").get(currency).asDouble();
+            return exchangeRate;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 1.0;
     }
 
     public List<MenuItemResponse> getAllMenuItems() {
